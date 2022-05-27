2588c01131baee8658cf8b986b9142d65d49a57f
43h <cw.clx@outlook.com>
2022-05-27 16:03:41 +0800

    add dpdk-22.03

---
+++ check.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+rst=`ls /proc`
+for i in $rst
+do 
+if [ -f /proc/$i/status ]
+then
+ 
+ if [ `cat /proc/$i/status | grep -i huge | wc -l` -gt 0 ]
+ then
+    echo "-----------------"
+	echo "pid: "$i 
+	cat /proc/$i/status | grep -i huge
+ fi
+fi
+done

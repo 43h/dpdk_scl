28517fd17b4f477f45560ef449c22264ad870b72
ubuntu <ubuntu@localhost.localdomain>
2022-06-18 15:02:47 +0800

    learn hello world

--- dpdk-22.03/examples/helloworld/main.c
+++ dpdk-22.03/examples/helloworld/main.c
@@ -33,7 +33,7 @@ main(int argc, char **argv)
 	int ret;
 	unsigned lcore_id;
 
-	ret = rte_eal_init(argc, argv);
+	ret = rte_eal_init(argc, argv);	//eal初始化
 	if (ret < 0)
 		rte_panic("Cannot init EAL\n");
 	/* >8 End of initialization of Environment Abstraction Layer */
@@ -41,18 +41,18 @@ main(int argc, char **argv)
 	/* Launches the function on each lcore. 8< */
 	RTE_LCORE_FOREACH_WORKER(lcore_id) {
 		/* Simpler equivalent. 8< */
-		rte_eal_remote_launch(lcore_hello, NULL, lcore_id);
+		rte_eal_remote_launch(lcore_hello, NULL, lcore_id);  //每个从核上启动任务
 		/* >8 End of simpler equivalent. */
 	}
 
 	/* call it on main lcore too */
-	lcore_hello(NULL);
+	lcore_hello(NULL); //主核也启动任务
 	/* >8 End of launching the function on each lcore. */
 
-	rte_eal_mp_wait_lcore();
+	rte_eal_mp_wait_lcore(); //主核任务结束后，等待从核任务结束，类似于父进程等待子进程的wait
 
 	/* clean up the EAL */
-	rte_eal_cleanup();
+	rte_eal_cleanup(); //清理eal
 
 	return 0;
 }
